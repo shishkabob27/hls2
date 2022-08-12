9648957c8b05dea21c739912a8a7f04c6c97e180
Xenthio <thegooster88@gmail.com>
2022-08-12 10:30:46 +1000

    Put in hammer category.

--- code/Entities/Monster/Scientist.cs
+++ code/Entities/Monster/Scientist.cs
@@ -1,4 +1,6 @@
 ﻿[Library("monster_scientist"), HammerEntity]
+[EditorModel("models/hl1/weapons/world/crowbar.vmdl")]
+[Title("Scientist"), Category("Monsters")]
 internal class Scientist : AnimatedEntity
 {
     // Stub, this does nothing yet
