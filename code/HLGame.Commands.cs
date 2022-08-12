c6bc25d62c63f2b2d47bd460fb6b0ac51d7a880f
Xenthio <thegooster88@gmail.com>
2022-08-12 17:20:49 +1000

    Add some useful commands.

--- code/HLGame.Commands.cs
+++ code/HLGame.Commands.cs
@@ -9,5 +9,44 @@
             sci.Position = ConsoleSystem.Caller.Pawn.Position;
 			sci.Spawn();
         }
+        
+		[ConCmd.Admin("respawn_entities")]
+		public static void RespawnEntities()
+		{
+			Map.Reset(DefaultCleanupFilter);
+		}
+
+		[ConCmd.Server("ent_create")]
+		public static void SpawnEntity(string entName)
+		{
+			var owner = ConsoleSystem.Caller.Pawn as Player;
+
+			if (owner == null)
+				return;
+
+			var entityType = TypeLibrary.GetTypeByName<Entity>(entName);
+			if (entityType == null)
+
+				if (!TypeLibrary.Has<SpawnableAttribute>(entityType))
+					return;
+
+			var tr = Trace.Ray(owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 200)
+				.UseHitboxes()
+				.Ignore(owner)
+				.Size(2)
+				.Run();
+
+			var ent = TypeLibrary.Create<Entity>(entityType);
+			if (ent is BaseCarriable && owner.Inventory != null)
+			{
+				if (owner.Inventory.Add(ent, true))
+					return;
+			}
+
+			ent.Position = tr.EndPosition;
+			ent.Rotation = Rotation.From(new Angles(0, owner.EyeRotation.Angles().yaw, 0));
+
+			//Log.Info( $"ent: {ent}" );
+		}
 	}
 }
