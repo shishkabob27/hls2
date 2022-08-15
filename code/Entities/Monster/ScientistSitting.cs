ddebfab9489c303d1d3d622b939de8a5819c7bf7
Xenthio <thegooster88@gmail.com>
2022-08-16 08:56:01 +1000

    Make sitting scientists not hover in air over the seat.

--- code/Entities/Monster/ScientistSitting.cs
+++ code/Entities/Monster/ScientistSitting.cs
@@ -34,7 +34,8 @@ internal class ScientistSitting : AnimatedEntity
         CurrentSequence.Name = Rand.FromList<string>(SittingAnims);
         //UseAnimGraph = false;
         SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
-        EnableHitboxes = true; 
+        EnableHitboxes = true;
+        Position = Position - new Vector3(0, 0, 24);
 
         Tags.Add("player"); // add player for now until a monster tag is added (can't do that now cause editing addon cfg is a pain for me (xenthio btw)
     
