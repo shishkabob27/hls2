ce495b0431b1b4ede64c11b3f30a24ed5a759ffe
Xenthio <thegooster88@gmail.com>
2022-08-16 08:57:22 +1000

    They shouldn't sit inside the chair either...

--- code/Entities/Monster/ScientistSitting.cs
+++ code/Entities/Monster/ScientistSitting.cs
@@ -35,7 +35,7 @@ internal class ScientistSitting : AnimatedEntity
         //UseAnimGraph = false;
         SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
         EnableHitboxes = true;
-        Position = Position - new Vector3(0, 0, 24);
+        Position = Position - new Vector3(0, 0, 22);
 
         Tags.Add("player"); // add player for now until a monster tag is added (can't do that now cause editing addon cfg is a pain for me (xenthio btw)
     
