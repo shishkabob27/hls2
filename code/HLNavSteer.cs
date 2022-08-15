d827d906d2e72ed2b955745cad306ec98fc8c279
Xenthio <thegooster88@gmail.com>
2022-08-16 08:53:40 +1000

    Fix avoiding

--- code/HLNavSteer.cs
+++ code/HLNavSteer.cs
@@ -27,7 +27,7 @@
 			Output.Direction = Path.GetDirection(currentPosition);
 		}
 
-		var avoid = GetAvoidance(currentPosition, 1);
+		var avoid = GetAvoidance(currentPosition, 32);
 		if (!avoid.IsNearlyZero())
 		{
 			Output.Direction = (Output.Direction + avoid).Normal;
