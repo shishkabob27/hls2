7158610c417f33eb85cc3b663ed788ef324a4d83
Xenthio <thegooster88@gmail.com>
2022-08-20 18:17:38 +1000

    lower the pathfinding accuracy back a tiny bit

--- code/Entities/legacy/scripted_sequence.cs
+++ code/Entities/legacy/scripted_sequence.cs
@@ -254,7 +254,7 @@ public partial class scripted_sequence : Entity
         {
             return;
         }
-        if (TargetNPC != null && (TargetNPC.Position.AlmostEqual(this.Position, 1f + (timeout / 1000)) || TargetNPC.Position == this.Position) && readyToPlay) //&& TargetNPC.CurrentSequence.IsFinished == true 
+        if (TargetNPC != null && (TargetNPC.Position.AlmostEqual(this.Position, 2 + (timeout / 1000)) || TargetNPC.Position == this.Position) && readyToPlay) //&& TargetNPC.CurrentSequence.IsFinished == true 
         {
             TargetNPC.Position = this.Position;
             timetick += 0.02f;
