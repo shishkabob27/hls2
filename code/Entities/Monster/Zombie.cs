c855ab09dd1ceaabca23026db8a2df5bafd11ae9
Xenthio <thegooster88@gmail.com>
2022-09-02 12:59:45 +1000

    Simple zombie, it will walk towards its target.

--- code/Entities/Monster/Zombie.cs
+++ code/Entities/Monster/Zombie.cs
@@ -4,21 +4,44 @@
 internal class Zombie : NPC
 {
     // Stub NPC, this does nothing yet
-
-    public override void Spawn()
+    public Zombie()
     {
-        base.Spawn();
+        NPCAnimGraph = "animgraphs/zombie.vanmgrph";
+        SetAnimGraph(NPCAnimGraph);
         Health = 20;
+        WalkSpeed = 30;
+        Speed = 30;
+    }
+    public override void Spawn()
+    { 
+
+        Health = 20;
+        base.Spawn();
         
         SetModel("models/hl1/monster/zombie.vmdl");
-        SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
+        //SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
         EnableHitboxes = true;
-        SetAnimGraph("animgraphs/zombie.vanmgrph");
         Tags.Add("npc", "playerclip");
     
     }
     public override void Think()
     {
+        if (LifeState != LifeState.Alive) return;
         base.Think();
+        // todo, trace a cone maybe...
+        var a = Trace.Ray(Position, Position + Rotation.Forward * 2000)
+            .Radius(30f)
+            .EntitiesOnly()
+            .Ignore(this)
+            .Run();
+        if (a.Entity == null) return;
+        var b = Trace.Ray(Position, a.Entity.Position)
+            .Ignore(this)
+            .Run();
+        if (b.Entity != a.Entity) return;
+        if (a.Entity is HLPlayer)
+        {
+            Steer.Target = a.Entity.Position;
+        }
     }
 }
