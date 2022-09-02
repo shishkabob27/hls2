b1d1e7638672e6fea34ef690c5eba7bc270e7d38
Xenthio <thegooster88@gmail.com>
2022-09-02 11:56:31 +1000

    simplify names

--- code/HLSurface.cs
+++ code/HLSurface.cs
@@ -121,10 +121,10 @@
 				case "flesh":
 					newName = "surface/hl_flesh.surface";
 					break;
-				case "hl_tile":
+				case "tile":
 					newName = "surface/hl_tile.surface";
 					break;
-				case "hl_metal":
+				case "metal":
 					newName = "surface/hl_metal.surface";
 					break;
 				default:
