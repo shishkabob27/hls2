5e648a7aa3391318c9f0af55a2337bef9502b82b
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-06 16:30:33 -0500

    Update ConVars

--- code/HLGame.ConVars.cs
+++ code/HLGame.ConVars.cs
@@ -2,7 +2,7 @@
 {
     // this can be accessed from anywhere!
 
-    [ConVar.Replicated] public static string hl_difficulty { get; set; } = "medium";
+    [ConVar.Replicated] public static string hl_skill { get; set; } = "medium";
     [ConVar.Replicated] public static string hl_gamemode { get; set; } = "campaign";
     [ConVar.Replicated] public static float hl_hud_scale { get; set; } = 0;
     [ConVar.Client] public static int cc_subtitles { get; set; } = 0;
