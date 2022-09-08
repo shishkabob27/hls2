bb4f5005b2a61d31cc509a97daebbe017dabaeec
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-08 16:34:40 -0500

    math_counter stub

---
+++ code/Entities/legacy/stub/math_counter.cs
@@ -0,0 +1,24 @@
+﻿[Library("math_counter")]
+[HammerEntity]
+[Title("math_counter"), Category("Legacy")]
+public partial class MathCounter : Entity
+{
+	// stub
+
+	[Property]
+	public float min { get; set; } = 0;
+
+	[Property]
+	public float max { get; set; } = 1;
+
+	[Property]
+	public float startvalue { get; set; } = 0;
+
+	protected Output OnHitMax { get; set; }
+
+	[Input]
+	public void Add()
+	{
+
+	}
+}
