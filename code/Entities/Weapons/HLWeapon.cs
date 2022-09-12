7e6d1a209ff7ff5b3b7d820d26b9aa392a283be2
Xenthio <thegooster88@gmail.com>
2022-09-12 12:20:24 +1000

    Crowbar collision

--- code/Entities/Weapons/HLWeapon.cs
+++ code/Entities/Weapons/HLWeapon.cs
@@ -562,7 +562,7 @@
 		Log.Info(vrmodel);
 		VRWeaponModel.SetModel(vrmodel);
         VRWeaponModel.SetupPhysicsFromModel(PhysicsMotionType.Keyframed);
-        VRWeaponModel.EnableAllCollisions = true;
+		VRWeaponModel.Tags.Add("vrweapon");
 		VRWeaponModel.PhysicsEnabled = true;
 		VRWeaponModel.UsePhysicsCollision = true;
         VRWeaponModel.SetAnimParameter("deploy", true);
