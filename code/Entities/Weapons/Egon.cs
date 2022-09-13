ca49f10b3ce4630873d79fe71a70320592af1ba0
Xenthio <thegooster88@gmail.com>
2022-09-13 14:28:59 +1000

    .....eh, looks kinda like the gluon gun.

--- code/Entities/Weapons/Egon.cs
+++ code/Entities/Weapons/Egon.cs
@@ -12,4 +12,70 @@ partial class Egon : HLWeapon
     public override AmmoType AmmoType => AmmoType.Uranium;
     public override string AmmoIcon => "ui/ammo7.png";
 
+    public override bool CanPrimaryAttack()
+    {
+        return base.CanPrimaryAttack();//Input.Pressed(InputButton.PrimaryAttack);
+    }
+    Particles Beam;
+    public override void Simulate(Client owner)
+    {
+        Log.Info("hi!");
+        if (!Input.Down(InputButton.PrimaryAttack))
+        {
+            if (Beam != null)
+            {
+                Beam.Destroy();
+                Beam = null;
+            }
+        }
+        if (Beam != null)
+        {
+
+            var owner2 = Owner as HLPlayer;
+            var startPos = GetFiringPos();
+            var dir = GetFiringRotation().Forward;
+            var tr = Trace.Ray(startPos, startPos + dir * 800)
+            .UseHitboxes()
+                .Ignore(owner2, false)
+                .WithAllTags("solid")
+                .Run();
+            Beam.SetPosition(1, tr.EndPosition);
+            Beam.SetPosition(1, tr.EndPosition);
+        }
+
+        base.Simulate(owner);
+    }
+    public override void AttackPrimary()
+    {
+
+        var owner = Owner as HLPlayer;
+        var startPos = GetFiringPos();
+        var dir = GetFiringRotation().Forward;
+        var tr = Trace.Ray(startPos, startPos + dir * 800)
+        .UseHitboxes()
+            .Ignore(owner, false)
+            .WithAllTags("solid")
+            .Run();
+        if (Beam == null)
+        {
+            Beam = Particles.Create("particles/egon_beam.vpcf", tr.EndPosition);
+        }
+
+        Beam.SetPosition(1, tr.EndPosition);
+        Beam.SetEntityAttachment(0, EffectEntity, "muzzle", true);
+        if (Client.IsUsingVr) Beam.SetEntityAttachment(0, VRWeaponModel, "muzzle", true);
+
+        Beam.SetPosition(0, (Vector3)GetAttachment("muzzle")?.Position);
+        Beam.SetForward(0, GetFiringRotation().Forward); 
+        //var pos = tr.StartPosition;
+        //var a = GetAttachment("muzzle");
+        //if (a != null)
+        //pos = (a ?? default).Position;
+        //Beam.SetPosition(0, pos);
+        Beam.SetPosition(1, tr.EndPosition);
+        Particles.Create("particles/gauss_impact.vpcf", tr.EndPosition);
+
+        base.AttackPrimary();
+    }
+
 }
