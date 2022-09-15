67e34c786387ad65f76b74acc1d42f7a5e63a897
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-14 19:00:57 -0500

    Replace ammo models

--- code/Entities/Ammo.cs
+++ code/Entities/Ammo.cs
@@ -41,69 +41,69 @@
 
 
 [Library( "ammo_9mmclip" ), HammerEntity]
-[EditorModel( "models/dm_ammo_9mmclip.vmdl" )]
+[EditorModel("models/hl1/weapons/ammo/w_9mmclip.vmdl")]
 [Title( "9mm Clip" ), Category( "Ammo" )]
 partial class Ammo9mmClip : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.Pistol;
 	public override int AmmoAmount => 17;
-	public override Model WorldModel => Model.Load( "models/dm_ammo_9mmclip.vmdl" );
+	public override Model WorldModel => Model.Load( "models/hl1/weapons/ammo/w_9mmclip.vmdl" );
 
 }
 
 [Library( "ammo_9mmbox" ), HammerEntity]
-[EditorModel( "models/dm_ammo_9mmbox.vmdl" )]
+[EditorModel("models/hl1/weapons/ammo/w_9mmbox.vmdl")]
 [Title( "9mm Box" ), Category( "Ammo" )]
 partial class Ammo9mmBox : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.Pistol;
 	public override int AmmoAmount => 200;
 
-	public override Model WorldModel => Model.Load( "models/dm_ammo_9mmbox.vmdl" );
+	public override Model WorldModel => Model.Load("models/hl1/weapons/ammo/w_9mmbox.vmdl");
 }
 
 
 
 [Library( "ammo_buckshot" ), HammerEntity]
-[EditorModel( "models/dm_ammo_buckshot.vmdl" )]
+[EditorModel("models/hl1/weapons/ammo/w_shotbox.vmdl")]
 [Title( "Buckshot" ), Category( "Ammo" )]
 partial class AmmoBuckshot : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.Buckshot;
 	public override int AmmoAmount => 12;
 
-	public override Model WorldModel => Model.Load( "models/dm_ammo_buckshot.vmdl" );
+	public override Model WorldModel => Model.Load("models/hl1/weapons/ammo/w_shotbox.vmdl");
 }
 
 [Library( "ammo_357" ), HammerEntity]
-[EditorModel( "models/dm_ammo_357.vmdl" )]
+[EditorModel("models/hl1/weapons/ammo/w_357ammobox.vmdl")]
 [Title( "357 Ammo" ), Category( "Ammo" )]
 partial class Ammo357 : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.Python;
 	public override int AmmoAmount => 6;
 
-	public override Model WorldModel => Model.Load( "models/dm_ammo_357.vmdl" );
+	public override Model WorldModel => Model.Load("models/hl1/weapons/ammo/w_357ammobox.vmdl");
 }
 
 [Library( "ammo_crossbow" ), HammerEntity]
-[EditorModel( "models/dm_ammo_crossbow.vmdl" )]
+[EditorModel("models/hl1/weapons/ammo/w_crossbow_clip.vmdl")]
 [Title( "Crossbow Bolts" ), Category( "Ammo" )]
 partial class AmmoCrossbow : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.Crossbow;
 	public override int AmmoAmount => 5;
 
-	public override Model WorldModel => Model.Load( "models/dm_ammo_crossbow.vmdl" );
+	public override Model WorldModel => Model.Load("models/hl1/weapons/ammo/w_crossbow_clip.vmdl");
 }
 
 [Library("ammo_ARgrenades"), HammerEntity]
-[EditorModel("models/dm_ammo_crossbow.vmdl")]
+[EditorModel("models/hl1/weapons/ammo/w_argrenade.vmdl")]
 [Title("SMG Grenade"), Category("Ammo")]
 partial class AmmoSMGGrenade : BaseAmmo
 {
 	public override AmmoType AmmoType => AmmoType.SMGGrenade;
 	public override int AmmoAmount => 3;
 
-	public override Model WorldModel => Model.Load("models/dm_ammo_crossbow.vmdl");
+	public override Model WorldModel => Model.Load("models/hl1/weapons/ammo/w_argrenade.vmdl");
 }
