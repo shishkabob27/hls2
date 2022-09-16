c651dc96709269b12084aefe122e15bf90f6e336
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-15 22:44:43 -0500

    Create gibshooter.cs

---
+++ code/Entities/legacy/stub/gibshooter.cs
@@ -0,0 +1,20 @@
+﻿[Library("gibshooter")]
+[HammerEntity]
+[EditorSprite("editor/gibshooter.vmat")]
+[Title("gibshooter"), Category("Legacy")]
+public partial class gibshooter : Entity
+{
+    // stub
+
+    [Property(Title = "Number of Gibs")]
+    public int m_iGibs { get; set; } = 1;
+
+    [Property(Title = "Gib Velocity")]
+    public int m_flVelocity { get; set; } = 1;
+
+    [Input]
+    public void Shoot()
+    {
+        HLCombat.CreateGibs(this.Position, this.Position, Health, new BBox(new Vector3(-16, -16, 0), new Vector3(16, 16, 72)));
+    }
+}
