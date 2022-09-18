25c1f1943c0ca1b06e04def56595a075c29ef040
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-18 16:13:53 -0500

    Spray stuff

--- code/HLGame.ConVars.cs
+++ code/HLGame.ConVars.cs
@@ -4,12 +4,16 @@
 
     [ConVar.Replicated] public static string hl_skill { get; set; } = "medium";
     [ConVar.Replicated] public static string hl_gamemode { get; set; } = "campaign";
-    [ConVar.Replicated] public static float hl_hud_scale { get; set; } = 0;
+    [ConVar.Client] public static float hl_hud_scale { get; set; } = 0;
     [ConVar.Replicated] public static bool hl_ragdoll { get; set; } = false;
+    [ConVar.Replicated] public static float sv_spray_max_distance { get; set; } = 100;
+	[ConVar.Replicated] public static float sv_spray_cooldown { get; set; } = 1;
     [ConVar.Client] public static bool hl_gui_rescale { get; set; } = true;
     [ConVar.Client] public static bool hl_pixelfont  { get; set; } = true;
     [ConVar.Client] public static int cc_subtitles { get; set; } = 0;
     [ConVar.Client] public static bool cl_himodels { get; set; } = false;
     [ConVar.Client] public static bool hl_vr_pointer { get; set; } = false;
+    [ConVar.Client] public static string hl_spray_icon { get; set; } = "lambda";
+    [ConVar.ClientData] public static string hl_spray_color { get; set; } = "orange";
     [ConVar.ClientData] public static bool hl_classic_flashlight { get; set; } = false;
 }
--- code/Player/HLPlayer.Spray.cs
+++ code/Player/HLPlayer.Spray.cs
@@ -2,23 +2,25 @@
 {
 	public TimeSince TimeSinceSprayed { get; set; }
 
+	static Color color = Color.Orange;
 	public static void Spray()
 	{
 		if ( ConsoleSystem.Caller.Pawn is HLPlayer player ) 
 		{
-			//if ( player.TimeSinceSprayed < sv_spray_cooldown ) 
-				//return;
+			if ( player.TimeSinceSprayed.Relative < HLGame.sv_spray_cooldown ) 
+				return;
 
-			var tr = Trace.Ray( player.EyePosition, player.EyePosition + player.EyeRotation.Forward * sv_spray_max_distance )
+			var tr = Trace.Ray( player.EyePosition, player.EyePosition + player.EyeRotation.Forward * HLGame.sv_spray_max_distance )
 				.WorldOnly()
 				.Run();
 
 			if ( tr.Hit )
 			{
 				Sound.FromWorld( "sprayer", tr.EndPosition );
-				if (ResourceLibrary.TryGet<DecalDefinition>("materials/hl1/spraypaint/lambda.decal", out var decal)){
-					Decal.Place(decal, tr);
 
+				var _ = "materials/hl1/spraypaint/"+HLGame.hl_spray_icon+".decal";
+				if (ResourceLibrary.TryGet<DecalDefinition>(_, out var decal)){
+					Decal.Place(decal, tr, color);
 					player.TimeSinceSprayed = 0;
 				}
 				else
@@ -29,7 +31,6 @@
 		}
 	}
 
-	[ConVar.Replicated] public static float sv_spray_max_distance { get; set; } = 100;
-	[ConVar.Replicated] public static float sv_spray_cooldown { get; set; } = 1;
+	
 
 }
