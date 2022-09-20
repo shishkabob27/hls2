9861deccee650bdabc34efa0cbb813e431778bb9
Xenthio <thegooster88@gmail.com>
2022-09-20 21:41:34 +1000

    gamemode weapon stubs for extended map support

--- code/Entities/Weapons/GamemodeStubs/dm98Stubs.cs
+++ code/Entities/Weapons/GamemodeStubs/dm98Stubs.cs
@@ -1,193 +1,200 @@
-﻿class BaseDM98Stub : Entity
+﻿class BaseDM98Stub : BaseGamemodeStub
 {
-    public override void Spawn()
-    {
-        if (false) // If we find any of our base entities from this gamemode we should abort.
-        {
-            Delete();
-            return;
-        }
-    }
 }
 
 
 // ---------------- WEAPONS ----------------
-[Library("dm_tripmine")]
+[Library( "dm_tripmine" )]
 class dm_tripmine : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new TripmineWeapon();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_smg")]
+[Library( "dm_smg" )]
 class dm_smg : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new SMG();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_357")]
+[Library( "dm_357" )]
 class dm_357 : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Python();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_pistol")]
+[Library( "dm_pistol" )]
 class dm_pistol : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Pistol();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_crossbow")]
+[Library( "dm_crossbow" )]
 class dm_crossbow : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Crossbow();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_crowbar")]
+[Library( "dm_crowbar" )]
 class dm_crowbar : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Crowbar();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_shotgun")]
+[Library( "dm_shotgun" )]
 class dm_shotgun : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Shotgun();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_grenade")]
+[Library( "dm_grenade" )]
 class dm_grenade : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new GrenadeWeapon();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 // ---------------- ITEMS ----------------
-[Library("dm_healthkit")]
+[Library( "dm_healthkit" )]
 class dm_healthkit : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new HealthKit();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_battery")]
+[Library( "dm_battery" )]
 class dm_battery : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Battery();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
 
-[Library("dm_chargerstation")]
+[Library( "dm_chargerstation" )]
 class dm_chargerstation : func_healthcharger
 {
 }
 
 
 // ---------------- AMMO ----------------
-[Library("dm_ammo9mmclip")]
+[Library( "dm_ammo9mmclip" )]
 class dm_ammo9mmclip : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Ammo9mmClip();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_ammo9mmbox")]
+[Library( "dm_ammo9mmbox" )]
 class dm_ammo9mmbox : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Ammo9mmAR();//new Ammo9mmBox();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_ammobuckshot")]
+[Library( "dm_ammobuckshot" )]
 class dm_ammobuckshot : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new AmmoBuckshot();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_ammo357")]
+[Library( "dm_ammo357" )]
 class dm_ammo357 : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new Ammo357();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
-[Library("dm_ammocrossbow")]
+[Library( "dm_ammocrossbow" )]
 class dm_ammocrossbow : BaseDM98Stub
 {
     public override void Spawn()
     {
-        base.Spawn();
+        if ( SpawnCheck() ) { Delete(); return; }
         var a = new AmmoCrossbow();
         a.Position = Position;
+        a.Tags.Add( "stubmade" );
         this.Delete();
     }
 }
