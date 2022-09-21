776d1dbd7e50386913e91f56bb5c70bc290a6bbb
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-21 17:21:14 -0500

    Create corpse on local client

--- code/Player/HLPlayer.cs
+++ code/Player/HLPlayer.cs
@@ -194,12 +194,7 @@
 		}
 		else
 		{
-			var a = Client.All.ToList();
-
-			a.RemoveAll(ply => ply == this.Owner);
-			// create corpse on everyones client except the client who died.
-			// maybe use hideinfirstperson on the client who died instead?
-            CreateCorpse(To.Multiple(a), Velocity, LastDamage.Flags, LastDamage.Position, LastDamage.Force, GetHitboxBone(LastDamage.HitboxIndex));
+            CreateCorpse(Velocity, LastDamage.Flags, LastDamage.Position, LastDamage.Force, GetHitboxBone(LastDamage.HitboxIndex), this);
 		}
 
 		Controller = null;
