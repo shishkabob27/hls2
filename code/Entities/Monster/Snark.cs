a1e5baecc58efe381da7e5fdeb2eb5169d1ad022
Xenthio <thegooster88@gmail.com>
2022-09-23 10:55:45 +1000

    use snark model

--- code/Entities/Monster/Snark.cs
+++ code/Entities/Monster/Snark.cs
@@ -4,4 +4,11 @@
 public class Snark : NPC
 {
 
+    public static readonly Model WorldModel = Model.Load( "models/hl1/weapons/world/squeak_npc.vmdl" );
+    public override void Spawn()
+    {
+        SetupPhysicsFromOBB( PhysicsMotionType.Static, new Vector3( 0.1f, 0.1f, 0.1f ), new Vector3( -0.1f, -0.1f, -0.1f ) );
+        base.Spawn();
+        Model = WorldModel;
+    }
 }
