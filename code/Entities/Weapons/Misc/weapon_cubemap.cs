32497203edfd256e8f16f50496e8a250c3791f1c
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-24 14:11:21 -0500

    Update weapon_cubemap.cs

--- code/Entities/Weapons/Misc/weapon_cubemap.cs
+++ code/Entities/Weapons/Misc/weapon_cubemap.cs
@@ -2,22 +2,5 @@
 [Title("weapon_cubemap"), Category( "Weapons" )]
 partial class weapon_cubemap : HLWeapon
 {
-	public static readonly Model WorldModel = Model.Load("models/hl1/weapons/world/glock.vmdl");
 	public override string ViewModelPath => "models/shadertest/envballs.vmdl";
-	
-	public override void Spawn()
-	{
-		base.Spawn();
-
-		Model = WorldModel;
-	}
-
-	public override bool CanPrimaryAttack()
-	{
-		return base.CanPrimaryAttack();
-	}
-
-	public override void AttackPrimary()
-	{
-	}
 }
