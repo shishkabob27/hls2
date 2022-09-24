8ca3450622f8f938def70f80f6f477e06202a4d8
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-23 23:14:58 -0500

    Create monstermaker.cs

---
+++ code/Entities/Monster/monstermaker.cs
@@ -0,0 +1,24 @@
+﻿[Library("monstermaker"), HammerEntity]
+[Title("monstermaker"), Category("Monsters"), Icon("person")]
+public class monstermaker : Entity
+{
+    //stub
+
+    [Property]
+	public string monstertype { get; set; }
+    
+
+    [Input]
+	public void Spawn()
+	{
+        var entityType = TypeLibrary.GetTypeByName<Entity>( monstertype );
+        if ( entityType == null )
+
+            if ( !TypeLibrary.Has<SpawnableAttribute>( entityType ) )
+                return;
+
+        var ent = TypeLibrary.Create<Entity>( entityType );
+
+        ent.Position = Position;
+	}
+}
