a0df8cb973f7036cdd4fa42ef1b65e68d50375c8
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-23 23:19:45 -0500

    Create env_shooter.cs

---
+++ code/Entities/legacy/env/env_shooter.cs
@@ -0,0 +1,20 @@
+﻿[Library("env_shooter")]
+[HammerEntity]
+[EditorSprite("editor/gibshooter.vmat")]
+[Title("env_shooter"), Category("Legacy"), Icon("dirty_lens")]
+public partial class env_shooter : Entity
+{
+    // stub
+
+    [Property(Title = "Number of Gibs")]
+    public int m_iGibs { get; set; } = 1;
+
+    [Property(Title = "Gib Velocity")]
+    public int m_flVelocity { get; set; } = 1;
+
+    [Input]
+    public void Shoot()
+    {
+        HLCombat.CreateGibs(this.Position, this.Position, Health, new BBox(new Vector3(-16, -16, 0), new Vector3(16, 16, 72)));
+    }
+}
