794af73221fe34355bb085a813e13155c7e98bb5
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-09-25 02:52:33 -0500

    Snark nest model

--- code/Entities/Weapons/HL1/SnarkWeapon.cs
+++ code/Entities/Weapons/HL1/SnarkWeapon.cs
@@ -4,7 +4,7 @@
 partial class SnarkWeapon : HLWeapon
 {
     //stub
-    public static readonly Model WorldModel = Model.Load( "models/hl1/weapons/world/snarknest.vmdl" );
+    public static readonly Model WorldModel = Model.Load( "models/hl1/weapons/world/sqknest.vmdl" );
     public override string ViewModelPath => "models/hl1/weapons/view/v_squeak.vmdl";
 
     public override int Bucket => 4;
