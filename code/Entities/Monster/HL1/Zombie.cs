cabc67d3a98b5b312b77d9e99d049bdbc2bbfdde
Xenthio <thegooster88@gmail.com>
2022-09-28 16:29:35 +1000

    Zombie attack viewpunch

--- code/Entities/Monster/HL1/Zombie.cs
+++ code/Entities/Monster/HL1/Zombie.cs
@@ -1,13 +1,13 @@
-﻿[Library("monster_zombie"), HammerEntity]
-[EditorModel("models/hl1/monster/zombie.vmdl")]
-[Title("Zombie"), Category("Monsters"), Icon("person")]
+﻿[Library( "monster_zombie" ), HammerEntity]
+[EditorModel( "models/hl1/monster/zombie.vmdl" )]
+[Title( "Zombie" ), Category( "Monsters" ), Icon( "person" )]
 internal class Zombie : NPC
 {
     // Stub NPC, this does nothing yet
     public Zombie()
     {
         NPCAnimGraph = "animgraphs/zombie.vanmgrph";
-        SetAnimGraph(NPCAnimGraph);
+        SetAnimGraph( NPCAnimGraph );
         Health = 20;
         WalkSpeed = 30;
         Speed = 30;
@@ -18,23 +18,23 @@ internal class Zombie : NPC
         Health = 20;
         base.Spawn();
 
-        SetModel("models/hl1/monster/zombie.vmdl");
+        SetModel( "models/hl1/monster/zombie.vmdl" );
         //SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
         EnableHitboxes = true;
-        Tags.Add("npc", "playerclip");
+        Tags.Add( "npc", "playerclip" );
 
     }
     public TimeSince TimeSinceLastSound = new TimeSince();
     public override void Think()
     {
 
-        int r = Rand.Int(50);
-        if (LifeState != LifeState.Alive) return;
-        if (r == 10 && TimeSinceLastSound > 5)
+        int r = Rand.Int( 50 );
+        if ( LifeState != LifeState.Alive ) return;
+        if ( r == 10 && TimeSinceLastSound > 5 )
         {
             TimeSinceLastSound = 0;
-            SpeakSound("sounds/hl1/zombie/zo_idle.sound");
-            
+            SpeakSound( "sounds/hl1/zombie/zo_idle.sound" );
+
         }
         /*
         if (TargetEntity is ICombat)
@@ -53,25 +53,25 @@ internal class Zombie : NPC
         }
         */
     }
-    public override void ProcessEntity(Entity ent, int rel)
+    public override void ProcessEntity( Entity ent, int rel )
     {
-        if (ent.LifeState != LifeState.Alive) return;
-        if (rel > 0 && ent.Position.Distance(Position) > 60)
+        if ( ent.LifeState != LifeState.Alive ) return;
+        if ( rel > 0 && ent.Position.Distance( Position ) > 60 )
         {
-            Steer.Target = ent.Position - ((ent.Position - Position).Normal * 50); // don't get too close!
+            Steer.Target = ent.Position - ( ( ent.Position - Position ).Normal * 50 ); // don't get too close!
         }
-        else if (rel > 0 && ent.Position.Distance(Position) < 60)
+        else if ( rel > 0 && ent.Position.Distance( Position ) < 60 )
         {
             animHelper.Attack = true;
         }
     }
-    public override void TakeDamage(DamageInfo info)
+    public override void TakeDamage( DamageInfo info )
     {
-        base.TakeDamage(info);
+        base.TakeDamage( info );
 
-        if (LifeState == LifeState.Alive)
+        if ( LifeState == LifeState.Alive )
         {
-            SpeakSound("sounds/hl1/zombie/zo_pain.sound");
+            SpeakSound( "sounds/hl1/zombie/zo_pain.sound" );
         }
 
     }
@@ -79,21 +79,30 @@ internal class Zombie : NPC
     {
         return (int)HLCombat.Class.CLASS_ALIEN_MONSTER;
     }
-    public override void OnAnimEventGeneric(string name, int intData, float floatData, Vector3 vectorData, string stringData)
+    public override void OnAnimEventGeneric( string name, int intData, float floatData, Vector3 vectorData, string stringData )
     {
-        if (stringData == "claw" && IsServer)
+        if ( stringData == "claw" && IsServer )
         {
-            foreach (var tr in TraceBullet(EyePosition, EyePosition + Rotation.Forward * 70, 1))
+            foreach ( var tr in TraceBullet( EyePosition, EyePosition + Rotation.Forward * 70, 1 ) )
             {
-                var damageInfo = DamageInfo.FromBullet(tr.EndPosition, Rotation.Forward * 50, 5)
-                    .UsingTraceResult(tr)
-                    .WithAttacker(this);
+                var damageInfo = DamageInfo.FromBullet( tr.EndPosition, Rotation.Forward * 50, 5 )
+                    .UsingTraceResult( tr )
+                    .WithAttacker( this );
 
 
-                Log.Info(damageInfo);
-                tr.Entity.TakeDamage(damageInfo);
+                Log.Info( damageInfo );
+                tr.Entity.TakeDamage( damageInfo );
+                if ( tr.Entity is HLPlayer player )
+                {
+                    var a = 18;
+                    if ( intData == 1 ) a = -18;
+                    var b = player.punchangle;
+                    b.z = a;
+                    b.x = 5;
+                    player.punchangle = b;
+                }
             }
         }
-        base.OnAnimEventGeneric(name, intData, floatData, vectorData, stringData);
+        base.OnAnimEventGeneric( name, intData, floatData, vectorData, stringData );
     }
 }
