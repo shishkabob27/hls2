1330a4756c5b9441c9baed45f4fc26c6c3cdd053
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-01 09:22:10 -0500

    Remove log

--- code/Entities/GamemodeStubs/GamemodeStubs.cs
+++ code/Entities/GamemodeStubs/GamemodeStubs.cs
@@ -5,7 +5,6 @@
         var b = Entity.All.OfType<HLWeapon>().ToList();
         b.RemoveAll( x => ( x as Entity ).Tags.Has( "stubmade" ) );
         b.RemoveAll( x => ( x as Entity ).Owner is HLPlayer );
-        Log.Info( b.Count() );
         if ( b.Count() > 2 ) // If we find any of our base entities from this gamemode we should abort.
         {
             Delete();
