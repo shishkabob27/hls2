e98ddd0996ef576588f05d19b2af7e263cd2c6ea
Xenthio <thegooster88@gmail.com>
2022-10-07 18:16:58 +1100

    actually good env_shake

--- code/Entities/Mapping/Effects/env_shake.cs
+++ code/Entities/Mapping/Effects/env_shake.cs
@@ -5,25 +5,54 @@ public partial class env_shake : Entity
 {
 	[Net]
 	public bool Active { get; set; } = false;
-	// stub
+
+
+	[Property( "amplitude" )]
+	public float Amplitude { get; set; } = 4;
+
+
+	[Property( "duration" )]
+	public float Duration { get; set; } = 1;
+
+	[Property( "frequency" )]
+	public float Frequency { get; set; } = 2.5f;
+
 	[Input]
 	void StartShake()
 	{
-		Active = true;
+		ShakeRPC();
+
 	}
+
 	[Input]
 	void StopShake()
 	{
-		Active = false;
+		StopShakeRPC();
 	}
 
-	[Event.Frame]
-	void Tick()
+	[ClientRpc]
+	void ShakeRPC()
 	{
-		if ( !Active ) return;
+
 		if ( Local.Pawn is not HLPlayer ply ) return;
 		if ( ply.CameraMode is not FirstPersonCamera plycam ) return;
-		plycam.ShakeOffset = new Vector3( Rand.Float( 1 ), Rand.Float( 1 ), Rand.Float( 1 ) );
-		Log.Info( "shakeyshakey!" );
+
+		plycam.Shake_AMPLITUDE = Math.Max( Amplitude, plycam.Shake_AMPLITUDE ); // avoid setting this lower if there is a stronger shake already active
+		plycam.Shake_DURATION = Math.Max( Duration, plycam.Shake_DURATION ); // avoid setting this lower if there is a stronger shake already active
+		plycam.Shake_FREQUENCY = Math.Max( Frequency, plycam.Shake_FREQUENCY ); // avoid setting this lower if there is a stronger shake already active
+		plycam.Shake_ENDTIME = Time.Now + plycam.Shake_DURATION;
+	}
+	[ClientRpc]
+	void StopShakeRPC()
+	{
+
+		if ( Local.Pawn is not HLPlayer ply ) return;
+		if ( ply.CameraMode is not FirstPersonCamera plycam ) return;
+
+		plycam.Shake_NEXTSHAKE = 0;
+		plycam.Shake_AMPLITUDE = 0;
+		plycam.Shake_DURATION = 0;
+		plycam.Shake_FREQUENCY = 0;
+		plycam.Shake_ENDTIME = 0;
 	}
 }
