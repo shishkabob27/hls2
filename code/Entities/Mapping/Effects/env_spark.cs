d7c3a73e6d09c35ca9081c5e3abd263210e09a69
Xenthio <thegooster88@gmail.com>
2022-10-07 13:11:51 +1100

    Initial env_spark implementation

---
+++ code/Entities/Mapping/Effects/env_spark.cs
@@ -0,0 +1,48 @@
+﻿[Library( "env_spark" )]
+[HammerEntity]
+[Title( "env_spark" ), Category( "Legacy" ), Icon( "volume_up" )]
+public partial class env_spark : Entity
+{
+
+
+	[Property( "MaxDelay" )]
+	public float MaxDelay { get; set; } = 1;
+
+	bool Active = false;
+	TimeSince LastSpark;
+	// stub
+	[Input]
+	void SparkOnce()
+	{
+		Particles.Create( "particles/spark.vpcf", Position );
+		LastSpark = 0;
+		OnSpark.Fire( this );
+	}
+
+	[Input]
+	void StartSpark()
+	{
+		Active = true;
+	}
+	[Input]
+	void StopSpark()
+	{
+		Active = false;
+	}
+	[Input]
+	void ToggleSpark()
+	{
+		Active = !Active;
+	}
+
+	[Event.Tick.Server]
+	void Tick()
+	{
+		if ( !Active ) return;
+		if ( LastSpark > MaxDelay ) SparkOnce();
+
+	}
+
+	protected Output OnSpark { get; set; }
+
+}
