9abb00ad24e4a8575a63daf41894ff13e69b67d6
Xenthio <thegooster88@gmail.com>
2022-10-10 09:56:24 +1100

    add internal spark entity

---
+++ code/Entities/spark.cs
@@ -0,0 +1,14 @@
+﻿class spark : Entity
+{
+	public virtual bool DoSound => true;
+	public override void Spawn()
+	{
+		Particles.Create( "particles/spark.vpcf", Position );
+		if ( DoSound )
+		{
+			Sound.FromWorld( "spark", Position );
+		}
+		base.Spawn();
+		Delete();
+	}
+}
