a8898581bb0473e520766b919174d1b864aed317
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-09 12:29:55 -0500

    Create infodecal.cs

---
+++ code/Entities/Mapping/stub/infodecal.cs
@@ -0,0 +1,11 @@
+﻿[Library("infodecal")]
+[HammerEntity]
+[Title("infodecal"), Category("Legacy")]
+public partial class infodecal : Entity
+{
+	// stub
+	
+	[Property, ResourceType("vmat")]
+	public string texture { get; set; }
+	
+}
