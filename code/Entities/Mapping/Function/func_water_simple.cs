22bbaf85187c48ba72abc47ce1c7f7ca0d45d247
Xenthio <thegooster88@gmail.com>
2022-10-10 15:51:45 +1100

    make this like actually work lol

--- code/Entities/Mapping/Function/func_water_simple.cs
+++ code/Entities/Mapping/Function/func_water_simple.cs
@@ -9,16 +9,58 @@
 [Title( "Water HL1" ), Category( "Gameplay" ), Icon( "water" )]
 public partial class func_water_simple : BrushEntity
 {
+	/// <summary>
+	/// Handles water level and buoyancy.
+	/// </summary>
+	public WaterController WaterController = new WaterController();
+	public func_water_simple()
+	{
+		Tags.Add( "water" );
+
+		WaterController.WaterEntity = this;
+		EnableTouch = true;
+		EnableTouchPersists = true;
+		RenderColor = RenderColor.WithAlpha( Math.Min( RenderColor.a, 0.95f ) );
+	}
+
 	public override void Spawn()
 	{
 		base.Spawn();
 
+		WaterController.WaterEntity = this;
 		Transmit = TransmitType.Always;
-
+		Tags.Clear();
+		Tags.Add( "water" );
 		CreatePhysics();
-		EnableDrawing = true;
+		RenderColor = RenderColor.WithAlpha( Math.Min( RenderColor.a, 0.95f ) );
+	}
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		WaterController.WaterEntity = null;
 	}
 
+	public override void Touch( Entity other )
+	{
+		base.Touch( other );
+
+		WaterController.Touch( other );
+	}
+
+	public override void EndTouch( Entity other )
+	{
+		base.EndTouch( other );
+
+		WaterController.EndTouch( other );
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		WaterController.StartTouch( other );
+	}
 	public override void ClientSpawn()
 	{
 		Host.AssertClient();
