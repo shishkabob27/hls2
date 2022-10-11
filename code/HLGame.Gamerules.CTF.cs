8ea47cd30627ee597153f0df8db7a6e764e34ba8
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-10 21:59:46 -0500

    Create HLGame.Gamerules.CTF.cs

---
+++ code/HLGame.Gamerules.CTF.cs
@@ -0,0 +1,48 @@
+﻿public partial class HLGame : Game
+{
+	[Net]
+    public static int ScoreTeamBM { get; set; } = 0;
+
+    [Net]
+    public static int ScoreTeamOF { get; set; } = 0;
+	
+	public static void MoveToCTFSpawnpoint( Entity pawn )
+	{
+		var spawnpoint = Entity.All
+								.OfType<info_player_deathmatch>()
+								.OrderByDescending( x => CTFSpawnpointWeight( pawn, x ) )
+								.ThenBy( x => Guid.NewGuid() )
+								.FirstOrDefault();
+
+		//Log.Info( $"chose {spawnpoint}" );
+
+		if ( spawnpoint == null )
+		{
+			Log.Warning( $"Couldn't find spawnpoint for {pawn}!" );
+			return;
+		}
+
+		pawn.Transform = spawnpoint.Transform;
+		pawn.Rotation = spawnpoint.Rotation;
+	}
+
+	public static float CTFSpawnpointWeight( Entity pawn, Entity spawnpoint )
+	{
+		float distance = 0;
+
+		foreach ( var client in Client.All )
+		{
+			if ( client.Pawn == null ) continue;
+			if ( client.Pawn == pawn ) continue;
+			if ( client.Pawn.LifeState != LifeState.Alive ) continue;
+
+			var spawnDist = ( spawnpoint.Position - client.Pawn.Position ).Length;
+			distance = MathF.Max( distance, spawnDist );
+		}
+
+		//Log.Info( $"{spawnpoint} is {distance} away from any player" );
+
+		return distance;
+	}
+
+}
