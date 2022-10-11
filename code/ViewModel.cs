b9539f36d67de2ee8dd506dfcfc2f5b69fd79489
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-10 22:18:44 -0500

    Update ViewModel.cs

--- code/ViewModel.cs
+++ code/ViewModel.cs
@@ -3,10 +3,6 @@
 
 	public override void PostCameraSetup( ref CameraSetup camSetup )
 	{
-		//base.PostCameraSetup( ref camSetup );
-
-		// camSetup.ViewModelFieldOfView = camSetup.FieldOfView + (FieldOfView - 80);
-
 		AddCameraEffects( ref camSetup );
 	}
 
@@ -19,22 +15,5 @@
 
 		if ( HLGame.CurrentState == HLGame.GameStates.GameEnd )
 			return;
-
-		//
-		// Bob up and down based on our walk movement
-		//
-		var speed = Owner.Velocity.Length.LerpInverse( 0, 800 );
-		var forward = camSetup.Rotation.Forward;
-
-		//if ( Owner.GroundEntity != null )
-		//{
-		//walkBob += Time.Delta * 25.0f * speed;
-		//}
-
-		//Position += forward * MathF.Sin( walkBob ) * speed * -1;
-	}
-	public override void Simulate( Client cl )
-	{
-		base.Simulate( cl );
 	}
 }
