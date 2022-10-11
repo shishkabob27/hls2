c0c325d7994252ed517921bf91ae097f57fb1333
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-10 21:59:43 -0500

    Create HLGame.Gamerules.Campaign.cs

---
+++ code/HLGame.Gamerules.Campaign.cs
@@ -0,0 +1,45 @@
+﻿public partial class HLGame : Game
+{
+	public override void MoveToSpawnpoint( Entity pawn )
+	{
+		var spawnpoint = Entity.All
+								.OfType<SpawnPoint>()
+								.OrderByDescending( x => SpawnpointWeight( pawn, x ) )
+								.ThenBy( x => Guid.NewGuid() )
+								.FirstOrDefault();
+
+		//Log.Info( $"chose {spawnpoint}" );
+
+		if ( spawnpoint == null )
+		{
+			Log.Warning( $"Couldn't find spawnpoint for {pawn}!" );
+			return;
+		}
+
+		pawn.Transform = spawnpoint.Transform;
+		pawn.Rotation = spawnpoint.Rotation;
+	}
+
+	/// <summary>
+	/// The higher the better
+	/// </summary>
+	public float SpawnpointWeight( Entity pawn, Entity spawnpoint )
+	{
+		float distance = 0;
+
+		foreach ( var client in Client.All )
+		{
+			if ( client.Pawn == null ) continue;
+			if ( client.Pawn == pawn ) continue;
+			if ( client.Pawn.LifeState != LifeState.Alive ) continue;
+
+			var spawnDist = ( spawnpoint.Position - client.Pawn.Position ).Length;
+			distance = MathF.Max( distance, spawnDist );
+		}
+
+		//Log.Info( $"{spawnpoint} is {distance} away from any player" );
+
+		return distance;
+	}
+
+}
