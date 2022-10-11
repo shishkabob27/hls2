99f3793e804ae5bfc42047f9ccc64abea12e6cf5
Xenthio <thegooster88@gmail.com>
2022-10-11 17:45:57 +1100

    timer shouldn't complain if it doesn't exist

--- code/UI/HUD/GameHud.cs
+++ code/UI/HUD/GameHud.cs
@@ -3,28 +3,32 @@ using Sandbox.UI.Construct;
 
 internal class GameHud : Panel
 {
-    public Label Timer;
-    public Label State;
+	public Label Timer;
+	public Label State;
 
-    public GameHud()
-    {
-        if ( !HLGame.GameIsMultiplayer() ) return;
-        State = Add.Label( string.Empty, "game-state" );
-        Timer = Add.Label( string.Empty, "game-timer" );
-    }
+	public GameHud()
+	{
+		if ( !HLGame.GameIsMultiplayer() ) return;
+		State = Add.Label( string.Empty, "game-state" );
+		Timer = Add.Label( string.Empty, "game-timer" );
+	}
 
-    public override void Tick()
-    {
-        if ( !HLGame.GameIsMultiplayer() ) return;
-        base.Tick();
+	public override void Tick()
+	{
+		if ( !HLGame.GameIsMultiplayer() ) return;
+		base.Tick();
 
-        var game = Game.Current as HLGame;
-        if ( !game.IsValid() ) return;
+		var game = Game.Current as HLGame;
+		if ( !game.IsValid() ) return;
+		try
+		{
+			var span = TimeSpan.FromSeconds( (game.StateTimer * 60).Clamp( 0, float.MaxValue ) );
 
-        var span = TimeSpan.FromSeconds( ( game.StateTimer * 60 ).Clamp( 0, float.MaxValue ) );
+			Timer.Text = span.ToString( @"hh\:mm\:ss" );
+			State.Text = game.GameState.ToString();
+		}
+		catch { }
 
-        Timer.Text = span.ToString( @"hh\:mm\:ss" );
-        State.Text = game.GameState.ToString();
-    }
+	}
 
 }
