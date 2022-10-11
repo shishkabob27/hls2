130decae7a877c29e9c097236a2e417770db9bd2
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-10 21:59:34 -0500

    Update HLGame.cs

--- code/HLGame.cs
+++ code/HLGame.cs
@@ -92,47 +92,6 @@ public partial class HLGame : Game
 		player.Respawn();
 	}
 
-	public override void MoveToSpawnpoint( Entity pawn )
-	{
-		var spawnpoint = Entity.All
-								.OfType<SpawnPoint>()
-								.OrderByDescending( x => SpawnpointWeight( pawn, x ) )
-								.ThenBy( x => Guid.NewGuid() )
-								.FirstOrDefault();
-
-		//Log.Info( $"chose {spawnpoint}" );
-
-		if ( spawnpoint == null )
-		{
-			Log.Warning( $"Couldn't find spawnpoint for {pawn}!" );
-			return;
-		}
-
-		pawn.Transform = spawnpoint.Transform;
-		pawn.Rotation = spawnpoint.Rotation;
-	}
-
-	/// <summary>
-	/// The higher the better
-	/// </summary>
-	public float SpawnpointWeight( Entity pawn, Entity spawnpoint )
-	{
-		float distance = 0;
-
-		foreach ( var client in Client.All )
-		{
-			if ( client.Pawn == null ) continue;
-			if ( client.Pawn == pawn ) continue;
-			if ( client.Pawn.LifeState != LifeState.Alive ) continue;
-
-			var spawnDist = ( spawnpoint.Position - client.Pawn.Position ).Length;
-			distance = MathF.Max( distance, spawnDist );
-		}
-
-		//Log.Info( $"{spawnpoint} is {distance} away from any player" );
-
-		return distance;
-	}
 
 	public override void OnKilled( Client client, Entity pawn )
 	{
