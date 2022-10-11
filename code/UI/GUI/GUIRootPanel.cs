4743a2a8f98ac294dbe324a77eeae72b37f25148
Xenthio <thegooster88@gmail.com>
2022-10-11 16:11:01 +1100

    Update GUIRootPanel.cs

--- code/UI/GUI/GUIRootPanel.cs
+++ code/UI/GUI/GUIRootPanel.cs
@@ -3,47 +3,47 @@
 [UseTemplate( "/resource/templates/ingamemenu.html" )]
 public class GUIRootPanel : RootPanel
 {
-    public bool MenuOpen;
-    public static GUIRootPanel Current;
-    public float OverrideScale { get; set; } = 0;
+	public bool MenuOpen;
+	public static GUIRootPanel Current;
+	public float OverrideScale { get; set; } = 0;
 
-    public GUIRootPanel()
-    {
-        AcceptsFocus = true;
-        Current = this;
-        StyleSheet.Load( "resource/styles/GUI.scss" );
-        Style.ZIndex = 100;
-        AddChild<Options>();
-        if ( HLGame.gamemode == "ctf" )
-        {
-            AddChild<TeamSelector>();
-        }
-        Focus();
-    }
+	public GUIRootPanel()
+	{
+		AcceptsFocus = true;
+		Current = this;
+		StyleSheet.Load( "resource/styles/GUI.scss" );
+		Style.ZIndex = 100;
+		AddChild<Options>();
+		if ( HLGame.sv_gamemode == "ctf" )
+		{
+			AddChild<TeamSelector>();
+		}
+		Focus();
+	}
 
-    public override void Tick()
-    {
-        base.Tick();
-    }
+	public override void Tick()
+	{
+		base.Tick();
+	}
 
-    protected override void UpdateScale( Rect screenSize )
-    {
-        Scale = 1;
-        if ( !HLGame.hl_gui_rescale ) return;
-        if ( OverrideScale > 0 )
-        {
-            Scale = OverrideScale;
-            return;
-        }
-        if ( Screen.Width > 1920 ) Scale = 1.50f;
-        if ( Screen.Height > 2650 ) Scale = 2.00f;
-    }
-    [Event.BuildInput]
-    public void ProcessClientInput( InputBuilder input )
-    {
-        if ( input.Pressed( InputButton.Menu ) )
-        {
-            MenuOpen = !MenuOpen;
-        }
-    }
+	protected override void UpdateScale( Rect screenSize )
+	{
+		Scale = 1;
+		if ( !HLGame.hl_gui_rescale ) return;
+		if ( OverrideScale > 0 )
+		{
+			Scale = OverrideScale;
+			return;
+		}
+		if ( Screen.Width > 1920 ) Scale = 1.50f;
+		if ( Screen.Height > 2650 ) Scale = 2.00f;
+	}
+	[Event.BuildInput]
+	public void ProcessClientInput( InputBuilder input )
+	{
+		if ( input.Pressed( InputButton.Menu ) )
+		{
+			MenuOpen = !MenuOpen;
+		}
+	}
 }
