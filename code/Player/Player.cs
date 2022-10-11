6efb60baf885af2635bf0ecc61026cc58e4a49dd
Xenthio <thegooster88@gmail.com>
2022-10-11 13:47:21 +1100

    Update Player.cs

--- code/Player/Player.cs
+++ code/Player/Player.cs
@@ -445,61 +445,7 @@
 		FootstepSounds();
 	}
 
-	TimeSince lastFootStep;
-	bool StepLeft;
-	void FootstepSounds()
-	{
-		if ( IsClient ) return;
-
-		float velrun;
-		float velwalk;
-		float flduck;
-		float fvol;
-
-		float speed = Velocity.Length;
-
-		if ( (IsDucked) || IsOnLadder )
-		{
-			velwalk = 60;       // These constants should be based on cl_movespeedkey * cl_forwardspeed somehow
-			velrun = 80;        // UNDONE: Move walking to server
-			flduck = 100;
-		}
-		else
-		{
-			velwalk = 120;
-			velrun = 210;
-			flduck = 0;
-		}
-		bool fWalking = speed < velrun;
-
-		if ( (IsOnLadder || (IsGrounded)) &&
-			(Velocity.Length > 0.0) &&
-			(speed >= velwalk) )
-		{
-
-
-			if ( lastFootStep > 0.35 && IsOnLadder )
-			{
-				lastFootStep = 0;
-				PlaySound( "pl_ladder" ).SetVolume( 0.5f );
-			}
-
-			if ( !HLGame.hl_legacyfootsteps ) return;
-			if ( IsOnLadder ) return;
-			if ( lastFootStep > (fWalking ? 0.4 : 0.3) )
-			{
-				StepLeft = !StepLeft;
-				lastFootStep = 0;
-				fvol = fWalking ? 0.2f : 0.5f;
-				if ( IsDucked )
-				{
-					if ( HLGame.hl_fix_ducking_footsteps ) fvol *= 0.35f; else return;
-					if ( HLGame.hl_fix_ducking_footsteps ) lastFootStep = -0.1f;
-				}
-				plfootstep( Position, fvol, (StepLeft ? 0 : 1) );
-			}
-		}
-	}
+	
 	Vector3 prevVel = Vector3.Zero;
 	const int PLAYER_FATAL_FALL_SPEED = 1024;// approx 20 metres
 	const int PLAYER_MAX_SAFE_FALL_SPEED = 580;// approx 5 metres
