815b6d8b7f74d4530e9f74b11e2f959b56081a6a
Xenthio <thegooster88@gmail.com>
2022-10-13 20:22:27 +1100

    change scaling

--- code/UI/HUD/HudRootPanel.cs
+++ code/UI/HUD/HudRootPanel.cs
@@ -2,20 +2,24 @@
 
 public class HudRootPanel : RootPanel
 {
-    public static HudRootPanel Current;
-    public HudRootPanel()
-    {
-        Current = this;
-    }
-    protected override void UpdateScale( Rect screenSize )
-    {
-        if ( HLGame.hl_hud_scale > 0 )
-        {
-            Scale = HLGame.hl_hud_scale;
-        }
-        else
-        {
-            base.UpdateScale( screenSize );
-        }
-    }
+	public static HudRootPanel Current;
+	public HudRootPanel()
+	{
+		Current = this;
+	}
+	protected override void UpdateScale( Rect screenSize )
+	{
+		if ( HLGame.hl_hud_scale > 0 )
+		{
+			Scale = HLGame.hl_hud_scale;
+
+		}
+		else
+		{
+			base.UpdateScale( screenSize );
+			if ( Screen.Width < 640 ) Scale = 0.5f;
+			if ( Screen.Width >= 640 ) Scale = 1.0f;
+			if ( Screen.Width >= 2650 ) Scale = 1.50f;
+		}
+	}
 }
