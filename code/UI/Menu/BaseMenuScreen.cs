ad8b65af4868c07b045a1a4a77518e1f3d3ef925
Xenthio <thegooster88@gmail.com>
2022-10-13 12:25:14 +1100

    SPend fucking AGES aligning this to be right

--- code/UI/Menu/BaseMenuScreen.cs
+++ code/UI/Menu/BaseMenuScreen.cs
@@ -9,15 +9,31 @@ public class BaseMenuScreen : Panel
 	public async Task BaseButtonClickDown( Panel p, bool doanim = true )
 	{
 		PlaySound( "launch_dnmenu1" );
-		if ( doanim ) AddClass( "justcldwn" );
-		if ( doanim ) await GameTask.DelaySeconds( 0.2f );
+		if ( doanim )
+		{
+			AddClass( "justcldwn" );
+			await GameTask.DelaySeconds( 0.2f );
+		}
 	}
-
+	[ConVar.Client]
+	public static float offsetX { get; set; } = 390;
+	[ConVar.Client]
+	public static float offsetY { get; set; } = 410;
 	public async Task BaseButtonClickUp( Panel p, bool doanim = true )
 	{
 		PlaySound( "launch_upmenu1" );
-		if ( doanim ) p.AddClass( "justcl" );
-		if ( doanim ) await GameTask.DelaySeconds( 0.2f );
+		if ( doanim )
+		{
+			p.AddClass( "justcl" );
+			var scale = 3.451f;
+			var scale2 = MenuRootPanel.Current.Scale;
+			var a = (0) + ((MenuRootPanel.Current.Box.Rect.Height / scale2) - (p.Box.Rect.Position.y / scale2)) - ((p.Box.Rect.Height / scale2) * (scale * 2));
+			var b = (0);
+			Log.Info( a );
+			p.Style.Top = a + offsetY;
+			p.Style.Left = b + offsetX;
+			await GameTask.DelaySeconds( 0.2f );
+		}
 	}
 
 	[ConCmd.Server]
