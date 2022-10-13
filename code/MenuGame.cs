895a67349938d4f0eda118b22c29d3ec28c5c4be
Xenthio <thegooster88@gmail.com>
2022-10-14 09:15:01 +1100

    Update MenuGame.cs

--- code/MenuGame.cs
+++ code/MenuGame.cs
@@ -1,4 +1,7 @@
-﻿public partial class MenuGame : Game
+﻿/// <summary>
+/// A simple version of the Game that loads just the menu and the GUI, used when the map is <empty
+/// </summary>
+public partial class MenuGame : Game
 {
 	[Net]
 	public MenuPanel Menu { get; set; }
