6ec01cf8c2b1e51701a57540a5f4ca4413f771c1
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-14 15:33:39 -0500

    whoops misinput

--- code/HLGame.ConVars.cs
+++ code/HLGame.ConVars.cs
@@ -29,7 +29,6 @@
 	}
 
 	[ConVar.Client] public static float hl_hud_scale { get; set; } = 0;
-	[ConVar.Client] public static string hl_hud_style { get; set; } = "hl1";
 	[ConVar.Replicated] public static bool hl_ragdoll { get; set; } = false;
 	[ConVar.Replicated] public static float sv_spray_max_distance { get; set; } = 100;
 	[ConVar.Replicated] public static float sv_spray_cooldown { get; set; } = 1;
@@ -39,7 +38,7 @@
 	[ConVar.Client] public static int cc_subtitles { get; set; } = 0;
 	[ConVar.Client] public static bool cl_himodels { get; set; } = false;
 	[ConVar.Client] public static bool cl_righthand { get; set; } = true;
-	[ConVar.ClientData] public static bool hl_classic_explosion { get; set; } = true;
+	[ConVar.ClientData] public static bool hl_classic_explosion { get; set; } = false;
 	[ConVar.Replicated] public static bool hl_classic_gibs { get; set; } = true;
 	[ConVar.Client] public static bool hl_vr_pointer { get; set; } = false;
 	[ConVar.ClientData] public static string hl_spray_icon { get; set; } = "lambda";
