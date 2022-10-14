67ef571cf3f627c2f5f64c470ba8043cf0a32ed8
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-13 23:06:40 -0500

    GameInfo type

--- code/GameInfo.cs
+++ code/GameInfo.cs
@@ -3,4 +3,5 @@ public class GameInfo
 	public static string game = "Half-Life";
 	public static string startmap = "shishkabob.hls2_c1p0";
 	public static string trainmap = "shishkabob.hls2_t0";
+	public static string type = "both"; //singleplayer_only, multiplayer_only, both
 }
