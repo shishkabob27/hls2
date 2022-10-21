59e87c61fc1d9c22de1877c5095d06d1fffaeef0
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-20 20:05:11 -0500

    vr crouch height option

--- code/ui/Menu/SubMenus/Config/VR.cs
+++ code/ui/Menu/SubMenus/Config/VR.cs
@@ -3,10 +3,39 @@
 [UseTemplate( "/UI/Menu/SubMenus/Config/VR.html" )]
 class VR : BaseMenuScreen
 {
+	public VR()
+	{
+		getCvars();
+	}
 	public void Done( Panel p )
 	{
+		updateCvars();
 		Delete();
 		var a = Parent.AddChild<Config>();
 		BaseButtonClickDown( p, a, true, "Virtual Reality" );
+	}
+
+	public bool vrpointer { get; set; } = false;
+	public int vrcrouchheight { get; set; } = 45;
+
+	void getCvars()
+	{
+		vrpointer = HLGame.hl_vr_pointer;
+		vrcrouchheight = HLGame.hl_vr_crouch_height;
+	}
+
+	public void updateCvars()
+	{
+		HLGame.hl_vr_pointer = vrpointer;
+		HLGame.hl_vr_crouch_height = vrcrouchheight;
+
+		ConsoleSystem.Run( "hl_vr_pointer " + (vrpointer ? 1 : 0) );
+		ConsoleSystem.Run( "hl_vr_crouch_height " + vrcrouchheight );
+		updtasync();
+	}
+	public async void updtasync()
+	{
+		await GameTask.DelaySeconds( 0.1f );
+		ConsoleSystem.Run( "hl_savecvar" );
 	}
 }
