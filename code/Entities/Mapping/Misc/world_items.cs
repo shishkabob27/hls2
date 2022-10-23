eb528744de8afcece886a5d37e3ce66812731b3e
Xenthio <thegooster88@gmail.com>
2022-10-23 21:53:35 +1100

    initial world_items implementation

---
+++ code/Entities/Mapping/Misc/world_items.cs
@@ -0,0 +1,25 @@
+[Library( "world_items" ), HammerEntity]
+[Title( "world_items" ), Icon( "place" )]
+public class world_items : Entity
+{
+	[Property]
+	public int Type { get; set; } = 0;
+	public override void Spawn()
+	{
+		base.Spawn();
+		var a = "ent";
+		switch ( Type )
+		{
+			case 42: a = "item_antidote"; break;
+			case 43: a = "item_security"; break;
+			case 44: a = "item_battery"; break;
+			case 45: a = "item_suit"; break;
+			default: break;
+		}
+		var entityType = TypeLibrary.GetDescription<Entity>( a );
+		if ( entityType == null ) return;
+		var b = entityType.Create<Entity>();
+		b.Position = this.Position;
+		b.Rotation = this.Rotation;
+	}
+}
