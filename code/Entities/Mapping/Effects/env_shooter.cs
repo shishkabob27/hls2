ce8926e7384a11941c4bc8614f448c61b1a7c04f
Xenthio <thegooster88@gmail.com>
2022-10-31 22:04:43 +1100

    Semi extended env_shooter

--- code/Entities/Mapping/Effects/env_shooter.cs
+++ code/Entities/Mapping/Effects/env_shooter.cs
@@ -12,9 +12,36 @@ public partial class env_shooter : Entity
     [Property(Title = "Gib Velocity")]
     public int m_flVelocity { get; set; } = 1;
 
+    [Property(Title = "Gib Variance")]
+    public int m_flVariance { get; set; } = 0;
+
     [Input]
     public void Shoot()
     {
-        HLCombat.CreateGibs(this.Position, this.Position, Health, new BBox(new Vector3(-16, -16, 0), new Vector3(16, 16, 72)));
-    }
+		for ( int i = 0; i < m_iGibs; i++ )
+		{
+			var gib = new HLGib();
+			gib.AngularVelocity = new Angles( Rand.Float( 100, 300 ), 0, Rand.Float( 100, 200 ) );
+			 
+			gib.Velocity = (Rotation.Forward + (Vector3.Random * m_flVariance)) * m_flVelocity;
+
+			if ( Health > -50 )
+			{
+				gib.Velocity = gib.Velocity * 0.7f;
+			}
+			else if ( Health > -200 )
+			{
+				gib.Velocity = gib.Velocity * 2;
+			}
+			else
+			{
+				gib.Velocity = gib.Velocity * 4;
+			}
+
+			gib.Position = Position;
+			gib.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+
+			gib.Spawn( 0 );
+		}
+	}
 }
--- code/Entities/Mapping/Effects/gibshooter.cs
+++ code/Entities/Mapping/Effects/gibshooter.cs
@@ -12,9 +12,38 @@ public partial class gibshooter : Entity
     [Property(Title = "Gib Velocity")]
     public int m_flVelocity { get; set; } = 1;
 
+    [Property(Title = "Gib Angular Velocity")]
+    public string gibanglevelocity { get; set; } = "";
+
     [Input]
     public void Shoot()
     {
-        HLCombat.CreateGibs(this.Position, this.Position, Health, new BBox(new Vector3(-16, -16, 0), new Vector3(16, 16, 72)));
-    }
+		for ( int i = 0; i < m_iGibs; i++ )
+		{
+			var gib = new HLGib();
+			gib.AngularVelocity = new Angles( Rand.Float( 100, 300 ), 0, Rand.Float( 100, 200 ) );
+
+			gib.Velocity = Rotation.Forward * -1;
+			gib.Velocity += new Vector3( Rand.Float( -0.25f, 0.25f ), Rand.Float( -0.25f, 0.25f ), Rand.Float( -0.25f, 0.25f ) );
+			gib.Velocity = gib.Velocity * Rand.Float( 300f, 400f );
+
+			if ( Health > -50 )
+			{
+				gib.Velocity = gib.Velocity * 0.7f;
+			}
+			else if ( Health > -200 )
+			{
+				gib.Velocity = gib.Velocity * 2;
+			}
+			else
+			{
+				gib.Velocity = gib.Velocity * 4;
+			}
+
+			gib.Position = Position;
+			gib.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+
+			gib.Spawn( 0 );
+		}
+	}
 }
