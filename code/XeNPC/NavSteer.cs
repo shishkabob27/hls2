1fa54432a7a1ac6172e8ecc644ee268462159067
Xenthio <thegooster88@gmail.com>
2022-11-02 08:59:44 +1100

    NavSteer Fixes

--- code/XeNPC/NavSteer.cs
+++ code/XeNPC/NavSteer.cs
@@ -51,12 +51,10 @@ public class NavSteer
 			var objectRadius = 200.0f;
 			//var draw = XeNPC.Debug.Draw.ForSeconds( 1 );
 			//draw.WithColor( Color.Red.WithAlpha( 1 ) ).Circle( ent.Position, Vector3.Up, objectRadius * 0.25f );
-			if (!(ent is ModelEntity) ) continue;
-			//draw.WithColor( Color.Green.WithAlpha( 1 ) ).Circle( ent.Position, Vector3.Up, objectRadius * 0.25f );
-			if ((ent is Weapon || ent is HLViewModel || ent is HLGib)) continue;  
-			if (ent is ModelEntity) objectRadius = ((ent as ModelEntity).CollisionBounds.Size.Length * 0.5f) + 64;
-			if (ent is BrushEntity ) objectRadius = ((ent as BrushEntity).CollisionBounds.Size.Length * 0.5f) + 64;
-			objectRadius = objectRadius.Clamp( 128, 2048 );
+			if (!(ent is ModelEntity) ) continue; 
+			if ((ent is Weapon || ent is HLViewModel || ent is HLGib || ent is ButtonEntity || ent is ButtonEntityRot) ) continue;  
+			if (ent is ModelEntity) objectRadius = ((ent as ModelEntity).CollisionBounds.Size.Length * 1.25f) + 0;
+			if (ent is BrushEntity ) objectRadius = ((ent as BrushEntity).CollisionBounds.Size.Length * 1.25f) + 0; 
 
 			//draw.WithColor( Color.White.WithAlpha( 1 ) ).Circle( ent.Position, Vector3.Up, objectRadius * 0.25f );
 
