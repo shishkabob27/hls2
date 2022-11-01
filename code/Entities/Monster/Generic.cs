7d1583b959b7e2196ce3a016266ed416c818f23d
TheBoxTrot <baehardman@gmail.com>
2022-11-01 14:10:14 +0800

    still bugged

--- code/Entities/Monster/Generic.cs
+++ code/Entities/Monster/Generic.cs
@@ -1,24 +1,31 @@
-﻿[Library("monster_generic"), HammerEntity]
+﻿[Library( "monster_generic" ), HammerEntity]
 [Model]
-[Title("monster_generic"), Category("Monsters"), Icon("person")]
+[Title( "monster_generic" ), Category( "Monsters" ), Icon( "person" )]
 public partial class GenericMonster : NPC
 {
-    // Stub NPC, this does nothing yet
+	// Stub NPC, this does nothing yet
 
-   // [Net, Property, ResourceType( "vmdl" )]
-   // public string model { get; set; }
+	// [Net, Property, ResourceType( "vmdl" )]
+	// public string model { get; set; }
 
-    public override void Spawn()
-    {
-        base.Spawn();
+	public override void Spawn()
+	{
+		base.Spawn();
 
 		// SetModel(model);
+		Health = 100;
 		SetupPhysicsFromModel( PhysicsMotionType.Keyframed, false );
 		//SetupPhysicsFromAABB(PhysicsMotionType.Keyframed, new Vector3(-16, -16, 0), new Vector3(16, 16, 72));
 		EnableHitboxes = true;
-        PhysicsEnabled = true;
-        UsePhysicsCollision = true;
+		PhysicsEnabled = true;
+		UsePhysicsCollision = true;
+		animHelper = new HLAnimationHelper( this );
+		Tags.Add( "npc", "playerclip" );
+		
+	}
+	[Event.Tick.Server]
+	public void Ticker()
+	{
 
-        Tags.Add("npc", "playerclip");
-    }
-}
+	}
+	}
