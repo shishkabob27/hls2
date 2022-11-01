c80a3f078316ee97aa9432030039d4dda55a26a4
Xenthio <thegooster88@gmail.com>
2022-11-01 12:20:22 +1100

    This does not need to be in a separate file

--- code/Entities/Mapping/Choreo/scripted_sequence.cs
+++ code/Entities/Mapping/Choreo/scripted_sequence.cs
@@ -136,4 +136,39 @@ public partial class scripted_sequence : Entity
 			Log.Info( $"[Scripted Sequence] {toprint}" );
 		}
 	}
+
+	public void MoveTo( MoveToMode moveMode )
+	{
+		DebugPrint( "Move to position started." );
+		switch ( moveMode )
+		{
+			case MoveToMode.No:
+				break;
+			case MoveToMode.Walk:
+				WalkTo();
+				break;
+			case MoveToMode.Run:
+				WalkTo( true );
+				break;
+			case MoveToMode.Custom_movement:
+				break;
+			case MoveToMode.Instantaneous:
+				TargetNPC.Position = Position;
+				TargetNPC.targetRotation = Rotation;
+				TargetNPC.targetRotationOVERRIDE = Rotation;
+				break;
+			case MoveToMode.No_turn_to_face:
+				TargetNPC.targetRotation = Rotation;
+				TargetNPC.targetRotationOVERRIDE = Rotation;
+				break;
+		}
+	}
+	async Task WalkTo( bool running = false )
+	{
+		DebugPrint( "Walking to position." );
+		TargetNPC.NPCTaskQueue.Enqueue( new MoveToTask( Position ) );
+		TargetNPC.NPCTaskQueue.Enqueue( new RotateToTask( Rotation ) );
+		//TargetNPC.Steer.Target = Position;
+		//TargetNPC.Speed = running ? TargetNPC.RunSpeed : TargetNPC.WalkSpeed;
+	}
 }
