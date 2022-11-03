0610b054cf99e823cb277ea06326118d1aec8a91
Xenthio <thegooster88@gmail.com>
2022-11-03 19:43:41 +1100

    more sprite stuff

--- code/Entities/Mapping/stub/env_sprite.cs
+++ code/Entities/Mapping/stub/env_sprite.cs
@@ -1,14 +1,16 @@
 ﻿[Library("env_sprite")]
 [HammerEntity]
-[Title("env_sprite"), Category("Effects"), Icon("volume_up")]
+[Title("env_sprite"), Category("Effects"), Icon("volume_up")] 
 public partial class env_sprite : RenderEntity
 {
 	[Property( "model" ), Net]
-	public string Sprite { get; set; } = "materials/dev/target_circle.vmat";
+	public string Sprite { get; set; } = "sprites/glow04.vmat";
 	public Material SpriteMaterial { get; set; } 
 
 	public float SpriteScale { get; set; } = 18f;
 
+	[Property("rendercolor"), Net]
+	Color SpriteColour { get; set; }
 	public bool Enabled { get; set; } = true;
 	public override void Spawn()
 	{
@@ -21,14 +23,20 @@ public partial class env_sprite : RenderEntity
 		if (SpriteMaterial == null)
 		{
 			var a = Sprite;
-			a = a.Replace( ".vmdl", ".vmat" );
-			a = a.Replace( "sprites/", "materials/hl1/sprites/" );
+			if (!a.Contains(".vmat"))
+			{
+				a = a.Replace( ".vmdl", ".vmat" );
+			}
+			if ( !a.Contains( "materials/" ) )
+			{
+				a = a.Replace( "sprites/", "materials/hl1/sprites/" );
+			}
 			Log.Info( a );
 			SpriteMaterial = Material.Load( a );
 		}
 		// Allow lights to affect the sprite
 		//Render.SetupLighting( obj );
-		Graphics.SetupLighting( obj );
+		Graphics.SetupLighting( obj );  
 
 		// Create the vertex buffer for the sprite
 		var vb = new VertexBuffer();
@@ -38,12 +46,13 @@ public partial class env_sprite : RenderEntity
 		var normal = CurrentView.Rotation.Backward;// Transform.PointToLocal( CurrentView.Position ).Normal;
 		var w = normal.Cross( Vector3.Down ).Normal;
 		var h = normal.Cross( w ).Normal;
-		float halfSpriteSize = SpriteScale / 2;
+		float halfSpriteSize = SpriteScale;
 
 		// Add a single quad to our vertex buffer
 		vb.AddQuad( new Ray( default, normal), w * halfSpriteSize, h * halfSpriteSize );
 
 		// Draw the sprite
-		vb.Draw( SpriteMaterial );
+		Graphics.Attributes.Set( "rendercolor", SpriteColour );
+		vb.Draw( SpriteMaterial ); 
 	}
 }
