3c20f50d1dbca5755ec1bdfbe2390a41599fc760
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-11-05 21:09:25 -0500

    readd most playermodels

---
+++ playermodel/hgrunt.pm
@@ -0,0 +1,8 @@
+<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->
+{
+	data = 
+	{
+		Model = resource:"models/hl1/player/hgrunt.vmdl"
+		Animgraph = resource:"animgraphs/hl1/player.vanmgrph"
+	}
+}
