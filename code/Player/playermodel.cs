fa85c51de66cfce98d14fe08d00b6ab7058fb311
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-11-05 21:03:48 -0500

    playermodel resource

---
+++ code/Player/playermodel.cs
@@ -0,0 +1,11 @@
+﻿[GameResource( "Playermodel", "pm", "" )]
+public partial class Playermodel : GameResource
+{
+
+	[ResourceType( "vmdl" )]
+	public string Model { get; set; }
+
+	[ResourceType( "vanmgrph" )]
+	public string Animgraph { get; set; }
+
+}
