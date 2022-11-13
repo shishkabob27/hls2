34d4d90529ddc40e61153196eea32e8965ea17cf
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-11-12 19:00:54 -0600

    Don't create desktop ui when in vr

--- code/UI/HUD/HudPanel.cs
+++ code/UI/HUD/HudPanel.cs
@@ -23,37 +23,39 @@ public class HudPanel : HudEntity<HudRootPanel>
 				_ = new VRInventory();
 				_ = new VRChat();
 			}
-
-			// Just display the HUD on-screen
-			StyleSheet.FromFile( "/resource/styles/hud.scss" );
-			RootPanel.SetTemplate( "/resource/templates/hud.html" );
-
-			RootPanel.AddChild<DamageIndicator>();
-			RootPanel.AddChild<Crosshair>();
-			RootPanel.AddChild<HitIndicator>();
-			RootPanel.AddChild<InventoryBar>();
-			RootPanel.AddChild<PickupFeed>();
-			RootPanel.AddChild<FlashlightUI>();
-			RootPanel.AddChild<Chat>();
-			RootPanel.AddChild<KillFeed>();
-			Scoreboard = RootPanel.AddChild<Scoreboard>();
-			RootPanel.AddChild<VoiceList>();
-			RootPanel.AddChild<VoiceSpeak>();
-			RootPanel.AddChild<Subtitle>();
-
-			if ( HLGame.sv_gamemode == "ctf" )
+			else
 			{
-				RootPanel.AddChild<FlagUI>();
-				RootPanel.AddChild<TeamSelector>();
+				// Just display the HUD on-screen
+				StyleSheet.FromFile( "/resource/styles/hud.scss" );
+				RootPanel.SetTemplate( "/resource/templates/hud.html" );
+
+				RootPanel.AddChild<DamageIndicator>();
+				RootPanel.AddChild<Crosshair>();
+				RootPanel.AddChild<HitIndicator>();
+				RootPanel.AddChild<InventoryBar>();
+				RootPanel.AddChild<PickupFeed>();
+				RootPanel.AddChild<FlashlightUI>();
+				RootPanel.AddChild<Chat>();
+				RootPanel.AddChild<KillFeed>();
+				Scoreboard = RootPanel.AddChild<Scoreboard>();
+				RootPanel.AddChild<VoiceList>();
+				RootPanel.AddChild<VoiceSpeak>();
+				RootPanel.AddChild<Subtitle>();
+
+				if ( HLGame.sv_gamemode == "ctf" )
+				{
+					RootPanel.AddChild<FlagUI>();
+					RootPanel.AddChild<TeamSelector>();
+				}
+
+				if ( HLGame.GameIsMultiplayer() )
+				{
+					RootPanel.AddChild<GameHud>();
+				}
+
+				if ( HLGame.sv_gamemode == "campaign" ) RootPanel.AddChild<ChapterText>();
 			}
-
-			if ( HLGame.GameIsMultiplayer() )
-			{
-				RootPanel.AddChild<GameHud>();
-			}
-
-			if ( HLGame.sv_gamemode == "campaign" ) RootPanel.AddChild<ChapterText>();
-
+			
 		}
 	}
 }
