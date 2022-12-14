fd03ea566a30650469c98415fc15919df1dcac99
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-12-13 23:27:38 -0600

    Remove lerp

--- code/ViewModel.cs
+++ code/ViewModel.cs
@@ -8,10 +8,8 @@
 
 	public void UpdateCamera()
 	{
-		var rotationDistance = Rotation.Distance( Camera.Rotation );
-
 		Position = Camera.Position;
-		Rotation = Rotation.Lerp( Rotation, Camera.Rotation, RealTime.Delta * rotationDistance * 1.1f );
+		Rotation = Camera.Rotation;
 
 		Camera.ZNear = 4;
 		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( 90f );
