f9639e5a11815ca2dd6bb895d9da1c138cdf5c19
Xenthio <thegooster88@gmail.com>
2023-07-18 20:50:43 +1000

    set the start map to c1a0 temporarily

--- code/GameInfo.cs
+++ code/GameInfo.cs
@@ -1,7 +1,7 @@
 public class GameInfo
 {
 	public static string game = "Half-Life";
-	public static string startmap = "shishkabob.hls2_c1p0";
+	public static string startmap = "shishkabob.hls2_c2p0";
 	public static string trainmap = "shishkabob.hls2_t0";
 	public static string type = "both"; //singleplayer_only, multiplayer_only, both
 }
